eed0d598847ea7c1aa6609351b7d08c42eef9ed3
ilyeSudo <houdjedje.ilyes@gmail.com>
2020-04-19 11:39:38 +0100

    Initial commit via create-react-native-app

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+
+class App extends Component {
+  state = {};
+  render() {
+    return <Text style={{ padding: 10 }}>Hello World</Text>;
+  }
+}
+
+export default App;
